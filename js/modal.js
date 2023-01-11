412fb879f12d398afb9d59c1dd106de32e515cf1
Verena Sousa <verena.ads19@hotmail.com>
2023-01-11 09:39:01 -0300

    Fechando o modal pelo o teclado

--- js/modal.js
+++ js/modal.js
@@ -17,4 +17,13 @@ export const Modal = {
 
 Modal.buttonClose.onclick = () => {
     Modal.close()
+}
+
+
+window.addEventListener('keydown', handleKeydown)
+
+function handleKeydown(event){
+    if(event.key === 'Escape'){
+        Modal.close()
+    }
 };
